156e334ec5c7a4fccb2ad0d2af68b760ce300ba8
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 16:39:42 +0300

    flask fun

--- exercises/app.py
+++ exercises/app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    pass
+    names = ["Miscar", "Steampunk", "Orbit", "Joker"]
+    return render_template(
+        "index.html",
+        names=names,
+        no_names = False)
 
 if __name__ == '__main__':
    app.run(debug = True)
